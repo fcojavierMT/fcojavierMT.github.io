0e03f4e61d4da67147b7c10df1a67791456f3ce9
fcojaviermt <fcojavierdev@gmail.com>
2023-05-07 11:21:36 +0200

    feat(links): added social links

--- src/components/AboutInfo/About.jsx
+++ src/components/AboutInfo/About.jsx
@@ -1,5 +1,34 @@
 import './About.css'
 import { Image } from '@chakra-ui/react'
+import { Icon } from '@chakra-ui/react'
+import { BsLinkedin, BsGithub, BsMailbox } from 'react-icons/bs'
+import { Link } from '@chakra-ui/react'
+
+import {
+  List,
+  ListItem,
+} from '@chakra-ui/react'
+
+const links = [
+  {
+    text: 'Linkedin',
+    icon: BsLinkedin,
+    href: 'https://es.linkedin.com/in/francisco-javier-mart%C3%ADnez-talavera-ba3a00180'
+  },
+  {
+    text: 'Github',
+    icon: BsGithub,
+    href: 'https://github.com/fcojavierMT'
+  }
+]
+
+const listItems = links.map((link) =>
+  <ListItem key={link}>
+    <Link href={link.href} isExternal>
+      <Icon as={link.icon} /> {link.text} 
+    </Link>
+  </ListItem>
+);
 
 function About() {
   return (
@@ -13,6 +42,20 @@ function About() {
         />
         <h1 className='title'>Francisco Javier Martínez Talavera</h1>
         <p className='occupation'>Frontend Developer</p>
+        <p className='description'>
+          I am a software developer with almost 5 years in the software industry.
+          <br />
+          Specialized in frontend and always eager to learn and improve every day.
+        </p>
+
+        <div className='find-me'>
+          <List spacing={3}>
+            {listItems}
+            <ListItem>
+              <Icon as={BsMailbox} /> <a href="mailto:fcojavierdev@gmail.com">fcojavierdev@gmail.com </a>
+            </ListItem>
+          </List>
+        </div>
       </div>
     </>
   )
